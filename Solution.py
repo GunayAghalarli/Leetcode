0b612f834f2a08b21d436983352ccd810408041a
Gunay Aghalarli <gunay.aghalarli@gmail.com>
2021-11-17 18:36:22 +0400

    Adding

--- Solution.py
+++ Solution.py
@@ -1,12 +1,11 @@
-roman = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
 class Solution:
-    def romanToInt(self, S: str) -> int:
-        summ= 0
-        for i in range(len(S)-1,-1,-1):
-            num = roman[S[i]]
-            if 3*num < summ: 
-                summ = summ-num
+    def romanToInt(self, s: str) -> int:
+        roman = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
+        res= 0
+        for i in range(len(s)):
+            
+            if i+1 < len(s) and roman[s[i+1]] > roman[s[i]]:
+                res = res- roman[s[i]]
             else: 
-                summ = summ+num
-        return summ
-        ####
+                res = res + roman[s[i]]
+        return res
